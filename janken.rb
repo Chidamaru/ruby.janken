a5af32466e9253af47a73421aab123d4aa6cfe4b
EC2 Default User <ec2-user@ip-172-31-38-225.us-west-2.compute.internal>
2022-08-03 22:43:43 +0000

    引き分けの場合はじゃんけんに戻るようにしました

--- janken.rb
+++ janken.rb
@@ -1,9 +1,11 @@
 
 
-
 puts "ジャンケン・・・(0:グー)or(1:チョキ)or(2:パー)"
 
 def janken
+    
+    
+    
 player_hand = gets.to_i
 program_hand = rand(0..2)
 janken = ["グー","チョキ","パー"]
@@ -24,6 +26,8 @@ else
     puts "ジャンケンは相手の勝ち"
     return false
 end
+
+
 end
 
 draw = true
@@ -31,10 +35,10 @@ while draw
     draw = janken
 end
 
-puts "---------------------------------------------------"
-puts "あっち向いて・・・・(0:上)or(1:下)or(2:右)or(3:左)"
 
 def next_game
+puts "---------------------------------------------------"
+puts "あっち向いて・・・・(0:上)or(1:下)or(2:右)or(3:左)"
 
 player_face = gets.to_i
 program_face = rand(0..3)
@@ -48,6 +52,9 @@ if player_face == program_face
     puts "あなたの勝ち！おめでとう！"
 else 
     puts "引き分け"
+    puts "ジャンケン・・・(0:グー)or(1:チョキ)or(2:パー)"
+    janken
+    next_game
 end
 
 
@@ -62,6 +69,9 @@ if player_face == program_face
     puts "あなたの負け！残念！"
 else 
     puts "引き分け"
+    puts "ジャンケン・・・(0:グー)or(1:チョキ)or(2:パー)"
+    janken
+    next_game
 end
 
 
@@ -69,12 +79,9 @@ end
 
 
 
-    
 end
 
 next_game
-
-
 
 
 
